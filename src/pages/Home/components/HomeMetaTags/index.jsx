716024cf03d12b54a30e9bebb14db03db914a0e4
luanbitar <7@lbitar.com>
2020-05-06 19:18:19 -0300

    feat: improving seo

---
+++ src/pages/Home/components/HomeMetaTags/index.jsx
@@ -0,0 +1,12 @@
+import Helmet from "react-helmet"
+
+function HomeMetaTags() {
+  return (
+    <Helmet>
+      <title>Portfolio</title>
+      <meta name="description" content="Hi, I'm a software developer." />
+    </Helmet>
+  )
+}
+
+export default HomeMetaTags
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
 import Layout from "components/Layout"
 import Welcome from "pages/Home/components/Welcome"
+import HomeMetaTags from "pages/Home/components/HomeMetaTags"
 
 const HomePage = () => {
-
   return (
     <Layout>
+      <HomeMetaTags />
       <Welcome />
     </Layout>
   )
