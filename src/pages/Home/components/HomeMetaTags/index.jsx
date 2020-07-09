2ef6f1a57f6411c926d3f829e342064f288b97a4
luanbitar <7@lbitar.com>
2020-07-09 14:36:02 -0300

    feat: removing canonical tag

--- src/pages/Home/components/HomeMetaTags/index.jsx
+++ src/pages/Home/components/HomeMetaTags/index.jsx
@@ -5,7 +5,6 @@ function HomeMetaTags() {
     <Helmet>
       <title>Luan Bitar</title>
       <meta name="description" content="Hi, I'm a software developer." />
-      <link rel="canonical" href="https://lbitar.com" />
     </Helmet>
   )
 }
